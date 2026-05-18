a8f9c5fe50a9b5ed9713739df45ddd5f218f59d1
Shiv Kapoor <shivkapoor1095@gmail.com>
2026-05-18 14:30:52 +0530

    feat:converted fct_incidents to incremental model

--- models/marts/fct_incidents.sql
+++ models/marts/fct_incidents.sql
@@ -1,14 +1,25 @@
+{{ config(
+    materialized='incremental',
+    unique_key='ticket_number'
+) }}
+
 with incidents as (
-SELECT ticket_number,
-       reassignment_count,
-       reopen_count,
-       modification_count,
-       made_sla,
-       is_active,
-       opened_at,
-       resolved_at,
-       closed_at,
-       (resolved_at - opened_at) as resolution_time
-FROM {{ref('int_incident_event_final_state')}}
+    select
+        ticket_number,
+        reassignment_count,
+        reopen_count,
+        modification_count,
+        made_sla,
+        is_active,
+        opened_at,
+        resolved_at,
+        closed_at,
+        (resolved_at - opened_at) as resolution_time
+    from {{ ref('int_incident_event_final_state') }}
+
+    {% if is_incremental() %}
+        where opened_at > (select max(opened_at) from {{ this }})
+    {% endif %}
 )
-SELECT * from incidents
+
+select * from incidents
